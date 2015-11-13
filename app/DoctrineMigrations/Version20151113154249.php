80a2c43f97cd8258df7772aa2d12928b5d366422
Oussema Ghodbane <oussema.ghodbane@it4pme.fr>
2015-11-13 16:36:58 +0100

    ajout nouvelle entite

---
+++ app/DoctrineMigrations/Version20151113154249.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151113154249 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE trajet (id INT AUTO_INCREMENT NOT NULL, depart INT NOT NULL, destination INT NOT NULL, distance INT NOT NULL, duree INT NOT NULL, date_creation DATE NOT NULL, date_modification DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE trajet');
+    }
+}
---
+++ src/Optimouv/FfbbBundle/Entity/Trajet.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Optimouv\FfbbBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Trajet
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Optimouv\FfbbBundle\Entity\TrajetRepository")
+ */
+class Trajet
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="depart", type="integer")
+     */
+    private $depart;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="destination", type="integer")
+     */
+    private $destination;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="distance", type="integer")
+     */
+    private $distance;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="duree", type="integer")
+     */
+    private $duree;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_creation", type="date")
+     */
+    private $dateCreation;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_modification", type="date")
+     */
+    private $dateModification;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set depart
+     *
+     * @param integer $depart
+     *
+     * @return Trajet
+     */
+    public function setDepart($depart)
+    {
+        $this->depart = $depart;
+
+        return $this;
+    }
+
+    /**
+     * Get depart
+     *
+     * @return integer
+     */
+    public function getDepart()
+    {
+        return $this->depart;
+    }
+
+    /**
+     * Set destination
+     *
+     * @param integer $destination
+     *
+     * @return Trajet
+     */
+    public function setDestination($destination)
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    /**
+     * Get destination
+     *
+     * @return integer
+     */
+    public function getDestination()
+    {
+        return $this->destination;
+    }
+
+    /**
+     * Set distance
+     *
+     * @param integer $distance
+     *
+     * @return Trajet
+     */
+    public function setDistance($distance)
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+
+    /**
+     * Get distance
+     *
+     * @return integer
+     */
+    public function getDistance()
+    {
+        return $this->distance;
+    }
+
+    /**
+     * Set duree
+     *
+     * @param integer $duree
+     *
+     * @return Trajet
+     */
+    public function setDuree($duree)
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    /**
+     * Get duree
+     *
+     * @return integer
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * Set dateCreation
+     *
+     * @param \DateTime $dateCreation
+     *
+     * @return Trajet
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation
+     *
+     * @return \DateTime
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set dateModification
+     *
+     * @param \DateTime $dateModification
+     *
+     * @return Trajet
+     */
+    public function setDateModification($dateModification)
+    {
+        $this->dateModification = $dateModification;
+
+        return $this;
+    }
+
+    /**
+     * Get dateModification
+     *
+     * @return \DateTime
+     */
+    public function getDateModification()
+    {
+        return $this->dateModification;
+    }
+}
+
