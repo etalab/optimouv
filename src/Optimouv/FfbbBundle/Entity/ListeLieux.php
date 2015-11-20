667f34fa59bb6b66805c4d5d3421669a3d9c1af4
Hendrik <hendrik@it4pme.fr>
2015-11-20 17:49:57 +0100

    create ListeLieux entity using annotation

---
+++ src/Optimouv/FfbbBundle/Entity/ListeLieux.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Optimouv\FfbbBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ListeLieux
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Optimouv\FfbbBundle\Entity\ListeLieuxRepository")
+ */
+class ListeLieux
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_utilisateur", type="integer")
+     */
+    private $idUtilisateur;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_creation", type="date")
+     */
+    private $dateCreation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lieux", type="text")
+     */
+    private $lieux;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return ListeLieux
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set idUtilisateur
+     *
+     * @param integer $idUtilisateur
+     *
+     * @return ListeLieux
+     */
+    public function setIdUtilisateur($idUtilisateur)
+    {
+        $this->idUtilisateur = $idUtilisateur;
+
+        return $this;
+    }
+
+    /**
+     * Get idUtilisateur
+     *
+     * @return integer
+     */
+    public function getIdUtilisateur()
+    {
+        return $this->idUtilisateur;
+    }
+
+    /**
+     * Set dateCreation
+     *
+     * @param \DateTime $dateCreation
+     *
+     * @return ListeLieux
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation
+     *
+     * @return \DateTime
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set lieux
+     *
+     * @param string $lieux
+     *
+     * @return ListeLieux
+     */
+    public function setLieux($lieux)
+    {
+        $this->lieux = $lieux;
+
+        return $this;
+    }
+
+    /**
+     * Get lieux
+     *
+     * @return string
+     */
+    public function getLieux()
+    {
+        return $this->lieux;
+    }
+}
+
