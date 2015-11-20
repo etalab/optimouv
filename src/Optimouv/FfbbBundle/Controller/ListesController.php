60dae09827b436a246cdd7ef7ccb8c9c9abae062
Hendrik <hendrik@it4pme.fr>
2015-11-20 17:34:55 +0100

    add listeParticipants and listeLieux using static string

--- src/Optimouv/FfbbBundle/Controller/ListesController.php
+++ src/Optimouv/FfbbBundle/Controller/ListesController.php
@@ -8,8 +8,23 @@ class ListesController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FfbbBundle:Listes:index.html.twig', array(
-                // ...
-            ));    }
+
+        $listeParticipants = array(
+            0 => "liste_equipes1",
+            1 => "liste_equipes2"
+        );
+
+        $listeLieux = array(
+            0 => "liste_terrains1",
+            1 => "liste_terrains2"
+
+        );
+
+        $outputListe =  array(
+            "listeParticipants" => $listeParticipants,
+            "listeLieux" => $listeLieux
+        );
+
+        return $this->render('FfbbBundle:Listes:index.html.twig', $outputListe);    }
 
 }
