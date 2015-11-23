3a07d85b420cb9b27c940d4240a632f80c06a51d
Oussema Ghodbane <oussema.ghodbane@it4pme.fr>
2015-11-23 11:13:29 +0100

    mise a jour schema base de donnees

---
+++ app/DoctrineMigrations/Version20151116112902.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151116112902 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE villes ADD id_ville INT NOT NULL, DROP code_postale, DROP nom');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE villes ADD code_postale VARCHAR(5) NOT NULL COLLATE utf8_unicode_ci, ADD nom VARCHAR(45) NOT NULL COLLATE utf8_unicode_ci, DROP id_ville');
+    }
+}
