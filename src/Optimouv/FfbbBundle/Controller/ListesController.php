27a06afb3dba6a69f2e3b89a83c7ae2cf21e80bc
Hendrik <hendrik@it4pme.fr>
2015-11-23 14:02:08 +0100

    view list of ListeLieux from DB

--- src/Optimouv/FfbbBundle/Controller/ListesController.php
+++ src/Optimouv/FfbbBundle/Controller/ListesController.php
@@ -8,23 +8,31 @@ class ListesController extends Controller
 {
     public function indexAction()
     {
+        $myfile = fopen("/tmp/ListeController_indexAction.log", "w") or die("Unable to open file!");
 
-        $listeParticipants = array(
+
+        # obtenir entity manager
+        $em = $this->getDoctrine()->getEntityManager();
+
+        # obtenir listes des lieux de rencontres
+        $listesLieux = $em->getRepository('FfbbBundle:ListeLieux')->getListes();
+
+        fwrite($myfile, "listesLieux: ".print_r($listesLieux, true));
+
+        $listesParticipants = array(
             0 => "liste_equipes1",
             1 => "liste_equipes2"
         );
 
-        $listeLieux = array(
-            0 => "liste_terrains1",
-            1 => "liste_terrains2"
-
+        $outputArray =  array(
+            "listesParticipants" => $listesParticipants,
+            "listesLieux" => $listesLieux
         );
+        fwrite($myfile, "outputArray: ".print_r($outputArray, true));
 
-        $outputListe =  array(
-            "listeParticipants" => $listeParticipants,
-            "listeLieux" => $listeLieux
-        );
 
-        return $this->render('FfbbBundle:Listes:index.html.twig', $outputListe);    }
+        return $this->render('FfbbBundle:Listes:index.html.twig', $outputArray);
+        fclose($myfile);
+    }
 
 }
