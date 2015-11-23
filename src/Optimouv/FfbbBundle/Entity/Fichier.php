3817e8ab77ad4eac0f7b8d43e65e49f40210b53c
Hendrik <hendrik@it4pme.fr>
2015-11-23 15:49:17 +0100

    add Entity Fichier and Repository Fichier

---
+++ src/Optimouv/FfbbBundle/Entity/Fichier.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Optimouv\FfbbBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Fichier
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Optimouv\FfbbBundle\Entity\FichierRepository")
+ */
+class Fichier
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="chemin", type="string", length=100)
+     */
+    private $chemin;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Fichier
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set chemin
+     *
+     * @param string $chemin
+     *
+     * @return Fichier
+     */
+    public function setChemin($chemin)
+    {
+        $this->chemin = $chemin;
+
+        return $this;
+    }
+
+    /**
+     * Get chemin
+     *
+     * @return string
+     */
+    public function getChemin()
+    {
+        return $this->chemin;
+    }
+}
+
