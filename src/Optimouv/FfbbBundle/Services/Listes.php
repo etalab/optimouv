2d765919ffda3377b66548dc66a09a60791b4094
Hendrik <hendrik@it4pme.fr>
2015-11-24 17:24:04 +0100

    add code to create a new ListeParticipants in service Listes

--- src/Optimouv/FfbbBundle/Services/Listes.php
+++ src/Optimouv/FfbbBundle/Services/Listes.php
@@ -8,13 +8,109 @@
 namespace Optimouv\FfbbBundle\Services;
 
 use SplFileObject;
+use PDO;
 
 class Listes{
 
 
-    public function uploadFichier()
+    private $database_name;
+    private $database_user;
+    private $database_password;
+    private $app_id;
+    private $app_code;
+
+    public function __construct($database_name, $database_user, $database_password, $app_id, $app_code)
     {
-        $myfile = fopen("/tmp/ListesService_uploadFichier.log", "w") or die("Unable to open file!");
+        $this->database_name = $database_name;
+        $this->database_user = $database_user;
+        $this->database_password = $database_password;
+        $this->app_id = $app_id;
+        $this->app_code = $app_code;
+    }
+
+    public function creerListe(){
+        $myfile = fopen("/tmp/ListesService_creerListe.log", "w") or die("Unable to open file!");
+
+        # insérer dans la base de données
+
+        $dbname = $this->database_name;
+        $dbuser = $this->database_user;
+        $dbpwd = $this->database_password;
+
+
+
+
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=' . $dbname . ';charset=utf8', $dbuser, $dbpwd);
+            fwrite($myfile, "pdo: ".print_r($bdd, true)."\n");
+
+        } catch (PDOException $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        if (!$bdd) {
+            //erreur de connexion!
+            die("\nPDO::errorInfo():\n");
+        } else {
+
+
+            date_default_timezone_set('Europe/Paris');
+            $dateCreation = date('Y-m-d', time());
+            $dateCreationNom = date('Ymd', time());
+
+            $sql = "SELECT count(*) as cnt FROM  liste_participants where date_creation = '$dateCreation' ;";
+            fwrite($myfile, "sql : ".print_r($sql, true)."\n");
+
+            $stmt = $bdd->prepare($sql);
+            $stmt->execute();
+            $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $nbrResultat = $resultat["cnt"];
+
+            fwrite($myfile, "resultat  : ".print_r($resultat , true)."\n");
+
+
+            $nomUtilisateur = "henz";
+
+            fwrite($myfile, "dateCreation: ".print_r($dateCreation, true)."\n");
+
+            $nom = "liste_equipe".$nbrResultat."_".$nomUtilisateur."_".$dateCreationNom;
+            $idUtilisateur = 1;
+            $equipes = "[1,2,3]";
+
+
+            $sql = "INSERT INTO  liste_participants (nom, id_utilisateur, date_creation, equipes) VALUES ( '$nom', '$idUtilisateur', '$dateCreation', '$equipes');\"";
+            fwrite($myfile, "sql : ".print_r($sql , true)."\n");
+            $insert = $bdd->prepare($sql);
+            $insert->execute();
+        }
+
+
+
+        $retour = json_encode(
+            array(
+                "success" => true,
+                "data" => "",
+            )
+        );
+
+        fwrite($myfile, "retour : ".print_r($retour , true)."\n");
+
+
+        return $retour;
+
+
+    }
+
+
+
+
+
+    public function creerEntites()
+    {
+
+
+        $myfile = fopen("/tmp/ListesService_creerEntites.log", "w") or die("Unable to open file!");
 
 
         $tempFilename = $_FILES["file-0"]["tmp_name"];
@@ -24,10 +120,10 @@ class Listes{
 
             // détecte le type de fichier
 
+
+
             // lire le contenu du fichier
-
-
-            $file = new \SplFileObject($tempFilename, 'r');
+            $file = new SplFileObject($tempFilename, 'r');
             $delimiter = ",";
 
             // On lui indique que c'est du CSV
@@ -43,40 +139,20 @@ class Listes{
 
 
 
-//            fwrite($myfile, "headerData: ".print_r($headerData, true));
-
-//        // Gestion des headers : présence ou non
-//        if ($request["header_opt"] == 1) {
-//
-//            // Obtient données des en-tetes
-//            $headerData = $file->fgetcsv();
-//
-//            // Données de l'aperçu
-//            $preview = $this->_preview($file);
-//
-//            // supprimer le fichier temporaire
-//            unlink($tempFile);
-//
-//            $str = json_encode(
-//                array(
-//                    "success" => true,
-//                    "data" => $headerData,
-//                    "lignes" => $preview
-//                )
-//            );
-//            echo $str;
-//            exit;
 
         }
 
+        $retour = json_encode(
+            array(
+                "success" => true,
+                "data" => $headerData,
+                )
+        );
 
 
-
-        $retour = "hello";
 
         return $retour;
     }
-
 
 
 
