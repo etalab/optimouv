175efaf07e2e88dfd3e7456ebe17584385e1de2c
Hendrik <hendrik@it4pme.fr>
2015-11-26 17:28:16 +0100

    add functions afficherLieuxAction and afficherParticipantsAction

--- src/Optimouv/FfbbBundle/Controller/GroupeController.php
+++ src/Optimouv/FfbbBundle/Controller/GroupeController.php
@@ -8,7 +8,62 @@ class GroupeController extends Controller
 {
     public function indexAction()
     {
-
         return $this->render('FfbbBundle:Groupe:index.html.twig');
     }
+
+    public function afficherParticipantsAction($idListeParticipants)
+    {
+        # obtenir entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        # obtenir listes des lieux de rencontres
+        $idParticipants = $em->getRepository('FfbbBundle:ListeParticipants')->getEquipesPourListe($idListeParticipants);
+        $idParticipants = $idParticipants[0]["equipes"];
+        $idParticipants = explode(",", $idParticipants);
+
+        # obtenir la date courante du système
+        date_default_timezone_set('Europe/Paris');
+        $dateTimeNow = date('Y-m-d_G:i:s', time());
+
+        # obtenir les détails pour chaque entité
+        $detailsEntites = [];
+        for($i=0; $i<count($idParticipants); $i++){
+            $detailsEntite = $em->getRepository('FfbbBundle:Entite')->getDetailsPourEntite($idParticipants[$i]);
+            array_push($detailsEntites,$detailsEntite );
+        }
+
+        error_log("\n Controller: Groupe, Function: afficherParticipantsAction, datetime: ".$dateTimeNow
+            ."\n detailsEntites : ".print_r($detailsEntites, true), 3, "/tmp/optimouv.log");
+
+        return $this->render('FfbbBundle:Groupe:indexUpdate.html.twig', $detailsEntites);
+    }
+
+    public function afficherLieuxAction($idListeLieux)
+    {
+        # obtenir entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        # obtenir listes des lieux de rencontres
+        $idLieux = $em->getRepository('FfbbBundle:ListeLieux')->getEquipesPourListe($idListeLieux);
+        $idLieux = $idLieux[0]["lieux"];
+        $idLieux = explode(",", $idLieux);
+
+        # obtenir la date courante du système
+        date_default_timezone_set('Europe/Paris');
+        $dateTimeNow = date('Y-m-d_G:i:s', time());
+
+        # obtenir les détails pour chaque entité
+        $detailsEntites = [];
+        for($i=0; $i<count($idLieux); $i++){
+            $detailsEntite = $em->getRepository('FfbbBundle:Entite')->getDetailsPourEntite($idLieux[$i]);
+            array_push($detailsEntites, $detailsEntite );
+        }
+
+        error_log("\n Controller: Groupe, Function: afficherParticipantsAction, datetime: ".$dateTimeNow
+            ."\n detailsEntites : ".print_r($detailsEntites, true), 3, "/tmp/optimouv.log");
+
+
+        return $this->render('FfbbBundle:Groupe:indexUpdate.html.twig', $detailsEntites);
+    }
+
 }
