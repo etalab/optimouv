da2a08427f69a8dbb5cbf0e1ac45553b8741b348
Hendrik <hendrik@it4pme.fr>
2015-11-27 16:22:59 +0100

    add new migration file

---
+++ app/DoctrineMigrations/Version20151127161859.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151127161859 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE liste_participants (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, id_utilisateur INT NOT NULL, date_creation DATE NOT NULL, equipes LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE liste_lieux (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, id_utilisateur INT NOT NULL, date_creation DATE NOT NULL, lieux LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE liste_participants');
+        $this->addSql('DROP TABLE liste_lieux');
+
+    }
+}
