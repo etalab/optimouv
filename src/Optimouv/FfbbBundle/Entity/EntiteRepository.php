ecfd23729cf1146c49feec6209868d8f28af43d1
Hendrik <hendrik@it4pme.fr>
2015-12-10 14:39:33 +0100

    getDetailsEntites return all info

--- src/Optimouv/FfbbBundle/Entity/EntiteRepository.php
+++ src/Optimouv/FfbbBundle/Entity/EntiteRepository.php
@@ -27,7 +27,9 @@ class EntiteRepository extends \Doctrine\ORM\EntityRepository
     {
 
         $query = $this->createQueryBuilder('e')
-            ->select('e.codePostal, e.ville', 'e.nom')
+            ->select('e.codePostal, e.ville', 'e.nom', 'e.typeEntite', 'e.prenom', 'e.adresse', 'e.longitude'
+                , 'e.latitude', 'e.projection', 'e.typeEquipement', 'e.nombreEquipement', 'e.capaciteRencontre'
+                , 'e.capacitePhaseFinale', 'e.participants', 'e.licencies', 'e.lieuRencontrePossible', 'e.idVilleFrance')
             ->where('e.id IN (:id)')
             ->setParameter('id', array_values($participants))
             ->orderBy('e.id','DESC')
