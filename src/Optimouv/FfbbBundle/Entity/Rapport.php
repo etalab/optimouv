b2b4b4009fb775aca8f490a47f7a930e3f424e77
Hendrik <hendrik@it4pme.fr>
2015-12-30 14:06:05 +0100

    add entity rapport

---
+++ src/Optimouv/FfbbBundle/Entity/Rapport.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Optimouv\FfbbBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rapport
+ *
+ * @ORM\Table(name="rapport")
+ * @ORM\Entity(repositoryClass="Optimouv\FfbbBundle\Entity\RapportRepository")
+ */
+class Rapport
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=100)
+     */
+    private $nom;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_groupe", type="integer")
+     */
+    private $idGroupe;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type_action", type="string", length=50)
+     */
+    private $typeAction;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="valeur_exclusion", type="integer")
+     */
+    private $valeurExclusion;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_creation", type="date")
+     */
+    private $dateCreation;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Rapport
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set idGroupe
+     *
+     * @param integer $idGroupe
+     *
+     * @return Rapport
+     */
+    public function setIdGroupe($idGroupe)
+    {
+        $this->idGroupe = $idGroupe;
+
+        return $this;
+    }
+
+    /**
+     * Get idGroupe
+     *
+     * @return integer
+     */
+    public function getIdGroupe()
+    {
+        return $this->idGroupe;
+    }
+
+    /**
+     * Set typeAction
+     *
+     * @param string $typeAction
+     *
+     * @return Rapport
+     */
+    public function setTypeAction($typeAction)
+    {
+        $this->typeAction = $typeAction;
+
+        return $this;
+    }
+
+    /**
+     * Get typeAction
+     *
+     * @return string
+     */
+    public function getTypeAction()
+    {
+        return $this->typeAction;
+    }
+
+    /**
+     * Set valeurExclusion
+     *
+     * @param integer $valeurExclusion
+     *
+     * @return Rapport
+     */
+    public function setValeurExclusion($valeurExclusion)
+    {
+        $this->valeurExclusion = $valeurExclusion;
+
+        return $this;
+    }
+
+    /**
+     * Get valeurExclusion
+     *
+     * @return integer
+     */
+    public function getValeurExclusion()
+    {
+        return $this->valeurExclusion;
+    }
+
+    /**
+     * Set dateCreation
+     *
+     * @param \DateTime $dateCreation
+     *
+     * @return Rapport
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation
+     *
+     * @return \DateTime
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+}
+
