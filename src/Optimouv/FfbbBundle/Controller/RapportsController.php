36db6d8a90a55e2eff7c27ddccb56c3e1d3ed0f1
Hendrik <hendrik@it4pme.fr>
2015-12-30 18:12:24 +0100

    display list of rapports (cannot yet launch report)

--- src/Optimouv/FfbbBundle/Controller/RapportsController.php
+++ src/Optimouv/FfbbBundle/Controller/RapportsController.php
@@ -8,6 +8,35 @@ class RapportsController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FfbbBundle:Rapports:index.html.twig');
+        # obtenir la date courante du système
+        date_default_timezone_set('Europe/Paris');
+        $dateTimeNow = date('Y-m-d_G:i:s', time());
+
+        # récupérer idUtilisateur
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $idUtilisateur = $user->getId();
+
+        # récupérer chemin fichier log du fichier parameters.yml
+        $this->error_log_path = $this->container->getParameter("error_log_path");
+
+        # obtenir entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        # récupérer tous les groupes appartenant à l'utilisateur courant
+        $groupesTemp = $em->getRepository('FfbbBundle:Groupe')->getGroupesParIdUtilisateur($idUtilisateur);
+        $idGroupes = [];
+        for($i=0; $i<count($groupesTemp); $i++){
+            array_push($idGroupes, $groupesTemp[$i]["id"]);
+        }
+
+        # récupérer tous les rapports de tous les groupes
+        $infoRapports = $em->getRepository('FfbbBundle:Rapport')->getRapportsParIdGroupe($idGroupes);
+
+//        error_log("\n Controller: Listes, Function: indexAction, datetime: ".$dateTimeNow
+//            ."\n infoRapports: ".print_r($infoRapports, true), 3, $this->error_log_path);
+
+        return $this->render('FfbbBundle:Rapports:index.html.twig', [
+            "infoRapports" => $infoRapports
+        ]);
     }
 }
