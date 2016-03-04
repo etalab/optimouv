40f9c1b939f5627a9b2af19d7ed84bfdb8d2ce55
Hendrik <hendrik@it4pme.fr>
2016-03-04 10:45:50 +0100

    patch migration symfony

--- app/DoctrineMigrations/Version20160302113614.php
+++ app/DoctrineMigrations/Version20160302113614.php
@@ -25,7 +25,7 @@ class Version20160302113614 extends AbstractMigration
         $this->addSql('ALTER TABLE resultats ADD CONSTRAINT FK_55ED970260A909EC FOREIGN KEY (id_rapport) REFERENCES parametres (id)');
         $this->addSql('DROP TABLE rapport');
         $this->addSql('DROP TABLE scenario');
-        $this->addSql('ALTER TABLE poule ADD CONSTRAINT FK_FA1FEB406E9E244D FOREIGN KEY (id_scenario) REFERENCES resultats (id)');
+//        $this->addSql('ALTER TABLE poule ADD CONSTRAINT FK_FA1FEB406E9E244D FOREIGN KEY (id_scenario) REFERENCES resultats (id)');
     }
 
     /**
@@ -37,7 +37,7 @@ class Version20160302113614 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE resultats DROP FOREIGN KEY FK_55ED970260A909EC');
-        $this->addSql('ALTER TABLE poule DROP FOREIGN KEY FK_FA1FEB406E9E244D');
+//        $this->addSql('ALTER TABLE poule DROP FOREIGN KEY FK_FA1FEB406E9E244D');
         $this->addSql('CREATE TABLE rapport (id INT AUTO_INCREMENT NOT NULL, id_groupe INT DEFAULT NULL, nom VARCHAR(100) NOT NULL COLLATE utf8_unicode_ci, type_action VARCHAR(50) NOT NULL COLLATE utf8_unicode_ci, valeur_exclusion INT NOT NULL, date_creation DATE NOT NULL, params LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci, statut TINYINT(1) DEFAULT NULL, INDEX IDX_BE34A09C228E39CC (id_groupe), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE scenario (id INT AUTO_INCREMENT NOT NULL, id_rapport INT DEFAULT NULL, nom VARCHAR(50) NOT NULL COLLATE utf8_unicode_ci, kilometres INT NOT NULL, duree INT NOT NULL, date_creation DATE NOT NULL, date_modification DATE NOT NULL, co2_voiture DOUBLE PRECISION NOT NULL, co2_covoiturage DOUBLE PRECISION NOT NULL, co2_minibus DOUBLE PRECISION NOT NULL, cout_voiture DOUBLE PRECISION NOT NULL, cout_covoiturage DOUBLE PRECISION NOT NULL, cout_minibus DOUBLE PRECISION NOT NULL, details_calcul LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci, INDEX id_rapport_idx (id_rapport), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE rapport ADD CONSTRAINT FK_BE34A09C228E39CC FOREIGN KEY (id_groupe) REFERENCES groupe (id) ON UPDATE CASCADE ON DELETE CASCADE');
