ee3e6efc33e799155c8da93728978d225913d150
Hendrik <hendrik@it4pme.fr>
2016-04-01 10:51:19 +0200

    refactor code

--- src/Optimouv/FfbbBundle/Controller/RencontresController.php
+++ src/Optimouv/FfbbBundle/Controller/RencontresController.php
@@ -754,7 +754,7 @@ use ZipArchive;
 
 
          # trier le tableau basé sur le nom de ville
-         $this->sksort($participants, "villeNom", true);
+         $this->get('service_rencontres')->sksort($participants, "villeNom", true);
 
 
 
@@ -780,33 +780,7 @@ use ZipArchive;
          return $retour;
      }
 
-     # fonction pour trier le tableau à partir d'un clé
-     private function sksort(&$array, $subkey="id", $sort_ascending=false) {
 
-        if (count($array))
-            $temp_array[key($array)] = array_shift($array);
-
-        foreach($array as $key => $val){
-            $offset = 0;
-            $found = false;
-            foreach($temp_array as $tmp_key => $tmp_val)
-            {
-                if(!$found and strtolower($val[$subkey]) > strtolower($tmp_val[$subkey]))
-                {
-                    $temp_array = array_merge(    (array)array_slice($temp_array,0,$offset),
-                                                array($key => $val),
-                                                array_slice($temp_array,$offset)
-                                              );
-                    $found = true;
-                }
-                $offset++;
-            }
-            if(!$found) $temp_array = array_merge($temp_array, array($key => $val));
-        }
-
-        if ($sort_ascending) $array = array_reverse($temp_array);
-        else $array = $temp_array;
-    }
 
 public function barycentreAction($idRapport)
     {
--- src/Optimouv/FfbbBundle/Services/Rencontres.php
+++ src/Optimouv/FfbbBundle/Services/Rencontres.php
@@ -1596,5 +1596,33 @@ class Rencontres
         return $nomRencontre;
     }
 
+    # fonction pour trier le tableau à partir d'un clé
+    public function sksort(&$array, $subkey="id", $sort_ascending=false) {
+
+        if (count($array))
+            $temp_array[key($array)] = array_shift($array);
+
+        foreach($array as $key => $val){
+            $offset = 0;
+            $found = false;
+            foreach($temp_array as $tmp_key => $tmp_val)
+            {
+                if(!$found and strtolower($val[$subkey]) > strtolower($tmp_val[$subkey]))
+                {
+                    $temp_array = array_merge(    (array)array_slice($temp_array,0,$offset),
+                        array($key => $val),
+                        array_slice($temp_array,$offset)
+                    );
+                    $found = true;
+                }
+                $offset++;
+            }
+            if(!$found) $temp_array = array_merge($temp_array, array($key => $val));
+        }
+
+        if ($sort_ascending) $array = array_reverse($temp_array);
+        else $array = $temp_array;
+    }
+
 
 }
