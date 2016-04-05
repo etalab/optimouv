68fd488879fef3d3ea5955207b0b49618a7752c3
Hendrik <hendrik@it4pme.fr>
2016-04-05 18:03:50 +0200

    initialize statistics page

---
+++ src/Optimouv/FfbbBundle/Controller/StatistiqueController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Optimouv\FfbbBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StatistiqueController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('FfbbBundle:Statistique:index.html.twig', array(
+                // ...
+            ));    }
+
+}
