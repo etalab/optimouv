7e12ed6aa3bc93df50f0e0ffd81dc2cf3796978a
Hendrik <hendrik@it4pme.fr>
2016-04-08 11:07:17 +0200

    draft function export pdf

--- src/Optimouv/FfbbBundle/Controller/StatistiqueController.php
+++ src/Optimouv/FfbbBundle/Controller/StatistiqueController.php
@@ -3,6 +3,7 @@
 namespace Optimouv\FfbbBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class StatistiqueController extends Controller
 {
@@ -12,6 +13,32 @@ class StatistiqueController extends Controller
 
         return $this->render('FfbbBundle:Statistique:index.html.twig', array(
             ));
+    }
+
+    public function exportPdfAction()
+    {
+
+//        return $this->render('FfbbBundle:Poules:testExportPdf.html.twig', [
+//        ]);
+
+        $html = $this->renderView('FfbbBundle:Statistique:index.html.twig', [
+        ]);
+
+
+        return new Response(
+            $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'attachment; filename="graph.pdf"',
+                'print-media-type'      => false,
+                'outline'               => true,
+
+            )
+        );
+
+
+
     }
 
 }
