97d8ebc5e3dae26e52a81b5b89fdcecaa5fee4a8
Hendrik <hendrik@it4pme.fr>
2016-04-12 09:59:39 +0200

    refactor code

--- src/Optimouv/AdminBundle/Services/LoginSuccessHandler.php
+++ src/Optimouv/AdminBundle/Services/LoginSuccessHandler.php
@@ -9,6 +9,8 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use PDO;
+use Optimouv\FfbbBundle\Services\Statistiques;
+
 
 /**
  * Custom authentication success handler
@@ -26,20 +28,28 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
      */
     protected $authorizationChecker;
 
+    /**
+     * @var Statistiques $serviceStatistiques
+     */
+    protected $serviceStatistiques;
 
-    private $error_log_path;
-    private $database_name;
-    private $database_user;
-    private $database_password;
 
-    public function __construct(Router $router, AuthorizationChecker $authorizationChecker, $database_name, $database_user, $database_password, $error_log_path)
+//    private $error_log_path;
+//    private $database_name;
+//    private $database_user;
+//    private $database_password;
+
+//    public function __construct(Router $router, AuthorizationChecker $authorizationChecker, $database_name, $database_user, $database_password, $error_log_path)
+    public function __construct(Router $router, AuthorizationChecker $authorizationChecker, Statistiques $serviceStatistiques)
     {
         $this->router = $router;
         $this->authorizationChecker = $authorizationChecker;
-        $this->database_name = $database_name;
-        $this->database_user = $database_user;
-        $this->database_password = $database_password;
-        $this->error_log_path = $error_log_path;
+        $this->serviceStatistiques = $serviceStatistiques;
+
+//        $this->database_name = $database_name;
+//        $this->database_user = $database_user;
+//        $this->database_password = $database_password;
+//        $this->error_log_path = $error_log_path;
     }
     /**
      * Called when authentication succeeds
@@ -54,7 +64,7 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
         # obtenir l'id de l'utilisateur
         $utilisateur =  $token->getUser();
         $utilisateurId = $utilisateur->getId();
-        error_log(" utilisateurId: $utilisateurId \n", 3, $this->error_log_path);
+//        error_log(" utilisateurId: $utilisateurId \n", 3, $this->error_log_path);
 
         if($utilisateurId == ""){
             error_log("\n  l'identifiant de l'utilisateur est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
@@ -66,60 +76,62 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
             || $this->authorizationChecker->isGranted('ROLE_USER'))
         {
             try{
-                # obtenir l'objet PDO
-                $pdo = $this->getPdo();
+                $this->serviceStatistiques->augmenterNombreTableStatistiques($utilisateurId, "nombreConnexions");
 
-                if (!$pdo) {
-                    //erreur de connexion
-                    error_log("\n erreur récupération de l'objet PDO, Service: LoginSuccessHandler, Function: onAuthenticationSuccess ", 3, $this->error_log_path);
-                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-                }
-
-                # obtenir l'id de la discipline
-                $sql = "SELECT id_discipline from fos_user where id=:id;";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':id', $utilisateurId);
-                $stmt->execute();
-                $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-                $disciplineId = $resultat["id_discipline"];
-
-                if($disciplineId == ""){
-                    error_log("\n  l'identifiant de la discipline est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
-                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-                }
-
-
-                # obtenir l'id de la fédération
-                $sql = "SELECT id_federation from discipline where id=:id;";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':id', $disciplineId);
-                $stmt->execute();
-                $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-                $federationId = $resultat["id_federation"];
-
-                if($federationId == ""){
-                    error_log("\n  l'identifiant de la fédération est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
-                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-                }
-
-
-                $typeStatistiques = "nombreConnexions";
-
-                # insérer dans la base de données
-                $sql = "INSERT INTO  statistiques_date (date_creation, type_statistiques, id_utilisateur, id_discipline, id_federation, valeur)
-                        VALUES (now(), :type_statistiques, :id_utilisateur, :id_discipline, :id_federation, 1)
-                        on duplicate key UPDATE valeur=valeur+1 ;";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':type_statistiques', $typeStatistiques);
-                $stmt->bindParam(':id_utilisateur', $utilisateurId);
-                $stmt->bindParam(':id_discipline', $disciplineId);
-                $stmt->bindParam(':id_federation', $federationId);
-                $statutInsert = $stmt->execute();
-
-                if(!$statutInsert){
-                    error_log("\n  Erreur d'insertion des données dans DB, details: ".print_r($stmt->errorInfo(), true)."\n Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
-                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-                }
+//                # obtenir l'objet PDO
+//                $pdo = $this->getPdo();
+//
+//                if (!$pdo) {
+//                    //erreur de connexion
+//                    error_log("\n erreur récupération de l'objet PDO, Service: LoginSuccessHandler, Function: onAuthenticationSuccess ", 3, $this->error_log_path);
+//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
+//                }
+//
+//                # obtenir l'id de la discipline
+//                $sql = "SELECT id_discipline from fos_user where id=:id;";
+//                $stmt = $pdo->prepare($sql);
+//                $stmt->bindParam(':id', $utilisateurId);
+//                $stmt->execute();
+//                $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+//                $disciplineId = $resultat["id_discipline"];
+//
+//                if($disciplineId == ""){
+//                    error_log("\n  l'identifiant de la discipline est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
+//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
+//                }
+//
+//
+//                # obtenir l'id de la fédération
+//                $sql = "SELECT id_federation from discipline where id=:id;";
+//                $stmt = $pdo->prepare($sql);
+//                $stmt->bindParam(':id', $disciplineId);
+//                $stmt->execute();
+//                $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+//                $federationId = $resultat["id_federation"];
+//
+//                if($federationId == ""){
+//                    error_log("\n  l'identifiant de la fédération est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
+//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
+//                }
+//
+//
+//                $typeStatistiques = "nombreConnexions";
+//
+//                # insérer dans la base de données
+//                $sql = "INSERT INTO  statistiques_date (date_creation, type_statistiques, id_utilisateur, id_discipline, id_federation, valeur)
+//                        VALUES (now(), :type_statistiques, :id_utilisateur, :id_discipline, :id_federation, 1)
+//                        on duplicate key UPDATE valeur=valeur+1 ;";
+//                $stmt = $pdo->prepare($sql);
+//                $stmt->bindParam(':type_statistiques', $typeStatistiques);
+//                $stmt->bindParam(':id_utilisateur', $utilisateurId);
+//                $stmt->bindParam(':id_discipline', $disciplineId);
+//                $stmt->bindParam(':id_federation', $federationId);
+//                $statutInsert = $stmt->execute();
+//
+//                if(!$statutInsert){
+//                    error_log("\n  Erreur d'insertion des données dans DB, details: ".print_r($stmt->errorInfo(), true)."\n Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
+//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
+//                }
 
 
             }
