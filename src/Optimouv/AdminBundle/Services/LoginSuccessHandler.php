13650b014eee2ed450a4f85de9974de0c3f6b4af
Hendrik <hendrik@it4pme.fr>
2016-04-12 10:05:09 +0200

    clean code

--- src/Optimouv/AdminBundle/Services/LoginSuccessHandler.php
+++ src/Optimouv/AdminBundle/Services/LoginSuccessHandler.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
-use PDO;
 use Optimouv\FfbbBundle\Services\Statistiques;
 
 
@@ -33,23 +32,12 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
      */
     protected $serviceStatistiques;
 
-
-//    private $error_log_path;
-//    private $database_name;
-//    private $database_user;
-//    private $database_password;
-
-//    public function __construct(Router $router, AuthorizationChecker $authorizationChecker, $database_name, $database_user, $database_password, $error_log_path)
     public function __construct(Router $router, AuthorizationChecker $authorizationChecker, Statistiques $serviceStatistiques)
     {
         $this->router = $router;
         $this->authorizationChecker = $authorizationChecker;
         $this->serviceStatistiques = $serviceStatistiques;
 
-//        $this->database_name = $database_name;
-//        $this->database_user = $database_user;
-//        $this->database_password = $database_password;
-//        $this->error_log_path = $error_log_path;
     }
     /**
      * Called when authentication succeeds
@@ -64,7 +52,6 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
         # obtenir l'id de l'utilisateur
         $utilisateur =  $token->getUser();
         $utilisateurId = $utilisateur->getId();
-//        error_log(" utilisateurId: $utilisateurId \n", 3, $this->error_log_path);
 
         if($utilisateurId == ""){
             error_log("\n  l'identifiant de l'utilisateur est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
@@ -78,62 +65,6 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
             try{
                 $this->serviceStatistiques->augmenterNombreTableStatistiques($utilisateurId, "nombreConnexions");
 
-//                # obtenir l'objet PDO
-//                $pdo = $this->getPdo();
-//
-//                if (!$pdo) {
-//                    //erreur de connexion
-//                    error_log("\n erreur récupération de l'objet PDO, Service: LoginSuccessHandler, Function: onAuthenticationSuccess ", 3, $this->error_log_path);
-//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-//                }
-//
-//                # obtenir l'id de la discipline
-//                $sql = "SELECT id_discipline from fos_user where id=:id;";
-//                $stmt = $pdo->prepare($sql);
-//                $stmt->bindParam(':id', $utilisateurId);
-//                $stmt->execute();
-//                $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-//                $disciplineId = $resultat["id_discipline"];
-//
-//                if($disciplineId == ""){
-//                    error_log("\n  l'identifiant de la discipline est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
-//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-//                }
-//
-//
-//                # obtenir l'id de la fédération
-//                $sql = "SELECT id_federation from discipline where id=:id;";
-//                $stmt = $pdo->prepare($sql);
-//                $stmt->bindParam(':id', $disciplineId);
-//                $stmt->execute();
-//                $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-//                $federationId = $resultat["id_federation"];
-//
-//                if($federationId == ""){
-//                    error_log("\n  l'identifiant de la fédération est null, Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
-//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-//                }
-//
-//
-//                $typeStatistiques = "nombreConnexions";
-//
-//                # insérer dans la base de données
-//                $sql = "INSERT INTO  statistiques_date (date_creation, type_statistiques, id_utilisateur, id_discipline, id_federation, valeur)
-//                        VALUES (now(), :type_statistiques, :id_utilisateur, :id_discipline, :id_federation, 1)
-//                        on duplicate key UPDATE valeur=valeur+1 ;";
-//                $stmt = $pdo->prepare($sql);
-//                $stmt->bindParam(':type_statistiques', $typeStatistiques);
-//                $stmt->bindParam(':id_utilisateur', $utilisateurId);
-//                $stmt->bindParam(':id_discipline', $disciplineId);
-//                $stmt->bindParam(':id_federation', $federationId);
-//                $statutInsert = $stmt->execute();
-//
-//                if(!$statutInsert){
-//                    error_log("\n  Erreur d'insertion des données dans DB, details: ".print_r($stmt->errorInfo(), true)."\n Service: LoginSuccessHandler, Function: onAuthenticationSuccess", 3, $this->error_log_path);
-//                    die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-//                }
-
-
             }
             catch (PDOException $e){
                  error_log("\n erreur PDO, Service: LoginSuccessHandler, Function: onAuthenticationSuccess, erreur: ".print_r($e, true), 3, $this->error_log_path);
@@ -145,23 +76,5 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
         return $response;
     }
 
-    # retourner un objet PDO qu'on peut utiliser dans d'autres fonctions
-    private function getPdo(){
-        # récupérer les parametres de connexion
-        $dbname = $this->database_name;
-        $dbuser = $this->database_user;
-        $dbpwd = $this->database_password;
-
-        try {
-            # créer une objet PDO
-            $pdo = new PDO('mysql:host=localhost;dbname=' . $dbname . ';charset=utf8', $dbuser, $dbpwd);
-        }
-        catch (PDOException $e) {
-            error_log("\n Service: Poules, Function: getPdo, \n PDOException: ".print_r($e, true), 3, $this->error_log_path);
-            die('Une erreur interne est survenue. Veuillez recharger l\'application. ');
-        }
-
-        return $pdo;
-    }
 
 }
