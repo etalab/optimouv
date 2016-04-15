27badddb08aca870a8f7f6c9ed98ceb13283d041
Hendrik <hendrik@it4pme.fr>
2016-04-15 14:45:03 +0200

    statistics page, filter for report type and federation is working

--- src/Optimouv/FfbbBundle/Controller/StatistiqueController.php
+++ src/Optimouv/FfbbBundle/Controller/StatistiqueController.php
@@ -9,9 +9,48 @@ class StatistiqueController extends Controller
 {
     public function indexAction()
     {
+        # récupérer chemin fichier log du fichier parameters.yml
+        $this->error_log_path = $this->container->getParameter("error_log_path");
+
+        $autorisationChecker = $this->get('security.authorization_checker');
+        $em = $this->getDoctrine()->getManager();
+
+        # obtenir l'id de l'utilisateur
+        $utilisateur = $this->get("security.token_storage")->getToken()->getUser();
+        $utilisateurId = $utilisateur->getId();
+
+        # obtenir l'id de la discipline
+        $disciplineId = $this->get("service_statistiques")->getDisciplineId($utilisateurId);
+
+        # obtenir l'id de la fédération
+        $federationId = $this->get("service_statistiques")->getFederationId($disciplineId);
+
+
+//        error_log("\n utilisateurId: ".print_r($utilisateurId, true), 3, $this->error_log_path);
+//        error_log("\n disciplineId: ".print_r($disciplineId, true), 3, $this->error_log_path);
+//        error_log("\n federationId: ".print_r($federationId, true), 3, $this->error_log_path);
+
+
+
+        # controler le role de l'utilisateur
+        # pour l'admin générale
+        if($autorisationChecker->isGranted("ROLE_SUPER_ADMIN")){
+            # obtenir la liste de toutes fédérations
+            $listeFederations = $this->get("service_statistiques")->getDetailsFederation("tous");
+        }
+        # pour l'admin fédérale
+        else{
+            # obtenir la fédération
+            $listeFederations = $this->get("service_statistiques")->getDetailsFederation($federationId);
+
+
+        }
+
+//        error_log("\n listeFederations: ".print_r($listeFederations, true), 3, $this->error_log_path);
 
 
         return $this->render('FfbbBundle:Statistique:index.html.twig', array(
+            "listeFederations" => $listeFederations
             ));
     }
 
