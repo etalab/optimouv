495886fe491ee4a9251d928ec9580b55db99b3db
Hendrik <hendrik@it4pme.fr>
2016-05-19 15:36:37 +0200

    add event listener which updates last_activity field for each user

---
+++ src/Optimouv/FfbbBundle/Services/ActivityListener.php
@@ -0,0 +1,53 @@
+<?php
+namespace Optimouv\FfbbBundle\Services;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Optimouv\AdminBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+
+class ActivityListener
+{
+    protected $authorizationChecker;
+    protected $em;
+    protected $tokenStorage;
+    protected $timeLimitActiveUsers;
+
+
+    public function __construct(AuthorizationChecker $authorizationChecker, TokenStorage $tokenStorage, EntityManager $manager, $timeLimitActiveUsers)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->tokenStorage = $tokenStorage;
+        $this->em = $manager;
+        $this->timeLimitActiveUsers = $timeLimitActiveUsers;
+    }
+
+    /**
+     * Update the user "lastActivity" on each request
+     * @param FilterControllerEvent $event
+     */
+    public function onCoreController(FilterControllerEvent $event)
+    {
+        // ici nous vérifions que la requête est une "MASTER_REQUEST" pour que les sous-requête soit ingoré (par exemple si vous faites un render() dans votre template)
+        if ($event->getRequestType() !== HttpKernel::MASTER_REQUEST) {
+            return;
+        }
+
+        // Nous vérifions qu'un token d'autentification est bien présent avant d'essayer manipuler l'utilisateur courant.
+        if ($this->tokenStorage->getToken()) {
+            $user = $this->tokenStorage->getToken()->getUser();
+
+            // Nous utilisons un délais pendant lequel nous considèrerons que l'utilisateur est toujours actif et qu'il n'est pas nécessaire de refaire de mise à jour
+            $delay = new \DateTime();
+            $delay->setTimestamp(strtotime($this->timeLimitActiveUsers." minutes ago"));
+
+            // Nous vérifions que l'utilisateur est bien du bon type pour ne pas appeler getLastActivity() sur un objet autre objet User
+            if ($user instanceof User && $user->getLastActivity() < $delay) {
+                $user->isActiveNow();
+                $this->em->flush($user);
+            }
+        }
+    }
+}
