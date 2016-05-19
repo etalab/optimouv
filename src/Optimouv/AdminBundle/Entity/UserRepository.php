bf30acb24e7cbabdd41d63c9303a7f0f43dc766a
Hendrik <hendrik@it4pme.fr>
2016-05-19 15:35:02 +0200

    add function getActive in userRepository

--- src/Optimouv/AdminBundle/Entity/UserRepository.php
+++ src/Optimouv/AdminBundle/Entity/UserRepository.php
@@ -115,6 +115,19 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
         
     }
 
+    public function getActive($timeLimit)
+    {
+        // Comme vous le voyez, le délais est redondant ici, l'idéale serait de le rendre configurable via votre bundle
+        $delay = new \DateTime();
+        $delay->setTimestamp(strtotime($timeLimit.' minutes ago'));
+
+        $qb = $this->createQueryBuilder('u')
+            ->where('u.lastActivity > :delay')
+            ->setParameter('delay', $delay)
+        ;
+
+        return $qb->getQuery()->getResult();
+    }
    
 
     
